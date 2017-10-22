9052ad9d8cf415ef0de6a5fa04b890e4c4b72ef4
Rafael Contreras <zirtrex@live.com>
2017-10-22 18:27:09 -0500

    Cambiando direccion de mongo en moduloModel.js

--- models/moduloModel.js
+++ models/moduloModel.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Squema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+mongoose.connect('mongodb://rafael:ZTRse7en@ds229465.mlab.com:29465/heroku_qzv4b77t', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 var moduloSquema = new Squema({ 
