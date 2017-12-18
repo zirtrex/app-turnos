06145770f7226fc6a6f27665e84d466d64307ace
Rafael Contreras <zirtrex@live.com>
2017-12-18 15:21:41 -0500

    make it better

--- models/moduloModel.js
+++ models/moduloModel.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Squema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:27017/test', { useMongoClient: true }); //mongodb://localhost:27017/test mongodb://rafael:ZTRse7en@ds229465.mlab.com:29465/heroku_qzv4b77t
+mongoose.connect('mongodb://rafael:ZTRse7en@ds229465.mlab.com:29465/heroku_qzv4b77t', { useMongoClient: true }); //mongodb://localhost:27017/test mongodb://rafael:ZTRse7en@ds229465.mlab.com:29465/heroku_qzv4b77t
 mongoose.Promise = global.Promise;
 
 var moduloSquema = new Squema({ 
