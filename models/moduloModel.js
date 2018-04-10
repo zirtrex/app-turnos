575dfbf374b98da473a5e037e3f0972be88ab428
Rafael Contreras <zirtrex@live.com>
2018-04-10 11:20:34 -0500

    Antes de empezar los reportes.

--- models/moduloModel.js
+++ models/moduloModel.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 var Squema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:27017/consuladoApp', { useMongoClient: true }); //mongodb://localhost:27017/mongodb://localhost:27017/consuladoApp mongodb://rafael:ZTRse7en@ds229465.mlab.com:29465/heroku_qzv4b77t
+mongoose.connect('mongodb://rafael:ZTRse7en@ds229465.mlab.com:29465/heroku_qzv4b77t', { useMongoClient: true }); 
+//mongodb://localhost:27017/consuladoApp mongodb://rafael:ZTRse7en@ds229465.mlab.com:29465/heroku_qzv4b77t
 mongoose.Promise = global.Promise;
 
 var moduloSquema = new Squema({ 
