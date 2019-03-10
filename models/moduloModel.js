156a242efe0322c3c7c95b37cc8ca91215130524
Rafael Contreras <zirtrex@live.com>
2019-03-10 03:28:18 -0500

    Cambio la conección para trabajar con heroku

--- models/moduloModel.js
+++ models/moduloModel.js
@@ -1,21 +1,21 @@
 var mongoose = require('mongoose');
 var Squema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:27017/consuladoApp', { useMongoClient: true }); 
+mongoose.connect('mongodb://rafael:ZTRse7en@ds229465.mlab.com:29465/heroku_qzv4b77t', { useMongoClient: true }); 
 //mongodb://localhost:27017/consuladoApp mongodb://rafael:ZTRse7en@ds229465.mlab.com:29465/heroku_qzv4b77t
 mongoose.Promise = global.Promise;
 
-var moduloSquema = new Squema({ 
+var moduloSquema = new Squema({
 	oficina: String,
 	servicio: String,
 	indicePerAtendidas: Number,
 	perAtendidas: [{
-		indiceAten: Number, 
-		fechaInicio: Date, 
-		fechaFin: Date, 
+		indiceAten: Number,
+		fechaInicio: Date,
+		fechaFin: Date,
 		fueAtendido: Boolean,
 		minutosAtendidos: Number}],
-	contador: Number,	
+	contador: Number,
 	fecha: String,
 	estado: Boolean
 }); //, { versionKey: false }
